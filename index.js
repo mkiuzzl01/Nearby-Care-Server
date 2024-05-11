b2bde734d9e144909715d88b73211bfbc72cc4f8
mkiuzzal01 <mkiuzzal07@gmail.com>
2024-05-12 00:33:45 +0600

    specific id related api is done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -28,6 +28,14 @@ async function run() {
     app.get('/Popular_Services', async(req,res)=>{
         const result = await allServiceCollection.find().toArray();
         res.send(result);
+    })
+    
+    app.get('/View_Details/:id', async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id:new ObjectId(id)}
+        const result = await allServiceCollection.findOne(query);
+        res.send(result)
+        
     })
 
     app.post('/Add_Appointment', async (req,res)=>{
